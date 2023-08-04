9f0763aa1a8bbc381353294ec3277edf8582b351
Gabriel Hübner Lucchesi <gabrielhubnerlucchesi@gmail.com>
2023-08-04 18:45:10 -0400

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from 'next/image'
+"use client";
+
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export default function Home() {
+  const {data} = useSession();
+
   return (
-    <h1>Homepage</h1>
+    <div>
+      <div className='flex flex-col'>
+        <button onClick={() => signIn()}>Login</button>
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+      <h1>Hey, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ""} alt=""/>
+    </div>
   )
 }
